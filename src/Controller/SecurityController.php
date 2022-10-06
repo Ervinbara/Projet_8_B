e97057979b48876690ba60221412ed78d11fc4d4
ebaraFFT <ebara@fft.fr>
2022-10-06 15:54:46 +0200

    Initial commit

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+//
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        dump($error);
+        //
+//        return $this->render('security/login.html.twig', array(
+//            'last_username' => $lastUsername,
+//            'error'         => $error,
+//        ));
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheck()
+    {
+        // This code is never executed.
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutCheck()
+    {
+        // This code is never executed.
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Table("user")
+ * @ORM\Entity
+ * @UniqueEntity("email")
+ * @UniqueEntity("username")
+ */
+class User implements UserInterface,PasswordAuthenticatedUserInterface
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     * @Assert\NotBlank(message="Vous devez saisir un nom d'utilisateur.")
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     * @Assert\NotBlank(message="Vous devez saisir une adresse email.")
+     * @Assert\Email(message="Le format de l'adresse n'est pas correcte.")
+     */
+    private $email;
+
+
+    /**
+     * @ORM\OneToMany(targetEntity="Task", mappedBy="author")
+     */
+    private $tasks;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    public $roles = [];
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getPassword():?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles():array
+    {
+        // Par défaut un nouvel utilisateur aura le rôle USER
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+
+    public function getTasks()
+    {
+        return $this->tasks;
+    }
+
+    public function addTask(Task $task)
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks[] = $task;
+            $task->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->id;
+    }
+}
